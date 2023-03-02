bc74299e0d9fabcba8c3235d06065c455a27524c
Nikcer <87930631+Nikcer@users.noreply.github.com>
2023-03-02 09:52:48 +0100

    creazione prgetto counter

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+// conteniner console del counter
+let container = document.createElement("div");
+container.setAttribute("id", "container");
+
+let buttonContainer = document.createElement("div");
+buttonContainer.setAttribute("id", "buttonContainer");
+
+//counter
+let counter = document.createElement("h3")
+counter.innerHTML = "0"
+counter.setAttribute("id","output")
+
+
+
+// buttons
+let buttonMinus = document.createElement("button");
+buttonMinus.innerText = "-";
+let buttonReset = document.createElement("button");
+buttonReset.innerText = "RESET";
+let buttonPlus = document.createElement("button");
+buttonPlus.innerText = "+";
+
+
+// buttons aggiunti al div
+buttonContainer.appendChild(buttonMinus);
+buttonContainer.appendChild(buttonReset);
+buttonContainer.appendChild(buttonPlus);
+
+
+//append container al body della pagina
+document.body.appendChild(container);
+//append buttonContainer e counter al container 
+container.appendChild(buttonContainer);
+container.appendChild(counter);
+
+ 
+// funzioni sottrazione,somma e reset counter
+
+
+buttonMinus.addEventListener("click", subnum);
+    function subnum(){
+        counter.innerHTML= parseInt(counter.innerHTML) - 1;
+
+}
+
+buttonPlus.addEventListener("click", addnum);
+    
+        function addnum (){
+            counter.innerHTML= parseInt(counter.innerHTML) + 1;       
+        }
+
+
+buttonReset.addEventListener("click", reset);
+        function reset (){
+            counter.innerHTML = "0";
+        }
+       
+    
