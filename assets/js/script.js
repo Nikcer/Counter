c12f628910061042638a32d0fce06581d43ecb1d
Mr-Zanzibar <104436099+Mr-Zanzibar@users.noreply.github.com>
2023-03-07 15:18:57 +0100

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,44 @@
-// contaniner console del counter
-let container = document.createElement("div");
+// definizione degli elementi del DOM
+const container = document.createElement("div");
 container.setAttribute("id", "container");
 
-//buttons container
-let buttonContainer = document.createElement("div");
+const buttonContainer = document.createElement("div");
 buttonContainer.setAttribute("id", "buttonContainer");
 
-//visualizzazione valore iniziale
-let counter = document.createElement("h3");
+const counter = document.createElement("h3");
 counter.innerHTML = 0;
-counter.setAttribute("id","output");
+counter.setAttribute("id", "output");
 
+const minusButton = document.createElement("button");
+minusButton.innerText = "-";
+const resetButton = document.createElement("button");
+resetButton.innerText = "RESET";
+const plusButton = document.createElement("button");
+plusButton.innerText = "+";
 
+// aggiunta degli elementi al DOM
+buttonContainer.appendChild(minusButton);
+buttonContainer.appendChild(resetButton);
+buttonContainer.appendChild(plusButton);
 
-// buttons
-let buttonMinus = document.createElement("button");
-buttonMinus.innerText = "-";
-let buttonReset = document.createElement("button");
-buttonReset.innerText = "RESET";
-let buttonPlus = document.createElement("button");
-buttonPlus.innerText = "+";
-
-
-// buttons aggiunti al div
-buttonContainer.appendChild(buttonMinus);
-buttonContainer.appendChild(buttonReset);
-buttonContainer.appendChild(buttonPlus);
-
-
-//append container al body della pagina
-document.body.appendChild(container);
-//append buttonContainer e counter al container 
 container.appendChild(buttonContainer);
 container.appendChild(counter);
+document.body.appendChild(container);
 
- 
-// funzioni cambio valore del counter
-buttonMinus.addEventListener("click", subnum);
-    function subnum(){
-        counter.innerHTML= parseInt(counter.innerHTML) - 1;
-
+// definizione delle funzioni
+function subNum() {
+  counter.innerHTML = parseInt(counter.innerHTML) - 1;
 }
 
-buttonPlus.addEventListener("click", addnum);
-    
-        function addnum (){
-            counter.innerHTML= parseInt(counter.innerHTML) + 1;       
-        }
+function addNum() {
+  counter.innerHTML = parseInt(counter.innerHTML) + 1;
+}
 
+function reset() {
+  counter.innerHTML = 0;
+}
 
-buttonReset.addEventListener("click", reset);
-        function reset (){
-            counter.innerHTML = 0;
-        }
-       
-    
+// eventi dei bottoni
+minusButton.addEventListener("click", subNum);
+plusButton.addEventListener("click", addNum);
+resetButton.addEventListener("click", reset);
